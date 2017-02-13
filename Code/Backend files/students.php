437eb2c484ee560683b75af9cb2123c993d997f2
SPA <ambatisaipraneeth@gmail.com>
2017-02-13 10:26:24 -0600

    Update students.php

--- Code/Backend files/students.php
+++ Code/Backend files/students.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect("MYSQL5002.Smarterasp.net","9ab3ca_pranu","pintu848");
+mysql_connect("MYSQL5002.Smarterasp.net","9ab3ca_pranu","*******");
 mysql_select_db("db_9ab3ca_pranu");
  
 $q=mysql_query("SELECT * FROM students WHERE faculty='".$_REQUEST['username']."' and subject='".$_REQUEST['subject']."'");
